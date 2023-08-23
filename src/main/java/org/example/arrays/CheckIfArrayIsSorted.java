44c6ea6b70a42be5a80ee8d1fcf4227fb2b0de6d
vishesh-baghel <visheshbaghel99@gmail.com>
2023-08-23 10:02:48 +0530

    added recursive solution also to the problem

--- src/main/java/org/example/arrays/CheckIfArrayIsSorted.java
+++ src/main/java/org/example/arrays/CheckIfArrayIsSorted.java
@@ -6,8 +6,8 @@ public class CheckIfArrayIsSorted {
         int[] inputArray = {1, 2, 3, 4, 5};
         int[] inputArray2 = {2, 3, 1, 3, 4};
 
-        boolean isSortedHappyPath = checkIfArrayIsSorted(inputArray);
-        boolean isSortedSadPath = checkIfArrayIsSorted(inputArray2);
+        boolean isSortedHappyPath = checkIfArrayIsSortedUsingRecursion(inputArray, inputArray.length);
+        boolean isSortedSadPath = checkIfArrayIsSortedUsingRecursion(inputArray2, inputArray2.length);
         System.out.println("expected array is sorted, and you found that the array is " + (isSortedHappyPath ? "sorted": "not sorted"));
         System.out.println("expected array is not sorted, and you found that the array is " + (isSortedSadPath ? "sorted": "not sorted"));
     }
@@ -22,5 +22,11 @@ public class CheckIfArrayIsSorted {
         }
 
         return true;
+    }
+
+    private static boolean checkIfArrayIsSortedUsingRecursion(int[] inputArray, int size) {
+       if (size == 1 || size == 0) return true;
+       if (inputArray[size - 1] < inputArray[size - 2]) return false;
+       return checkIfArrayIsSortedUsingRecursion(inputArray, size - 1);
     }
 }
